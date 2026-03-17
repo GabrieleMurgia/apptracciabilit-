e6621cef8dbb8d15d4f714a8541d14a52f615886
Gabriele Murgia <gabriele.murgia.external@valentino.com>
2026-03-17 14:56:32 +0000

    dynamic year in the footer

--- webapp/Component.js
+++ webapp/Component.js
@@ -17,13 +17,34 @@ sap.ui.define([
              * @override
              */
             
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
-                this.getRouter().initialize();
+init: function () {
+    UIComponent.prototype.init.apply(this, arguments);
 
-                this.setModel(models.createDeviceModel(), "device");
-            }
+    // ── Dynamic year for legal footer ──
+    var sYear = String(new Date().getFullYear());
+
+    // Screen3/4/5/6: CSS ::before
+    var oStyle = document.createElement("style");
+    oStyle.textContent =
+      '.pageWithLegalBar .sapMPageFooter::before { content: "VALENTINO.COM\\APowered by Valentino Copyright © ' +
+      sYear +
+      ' VALENTINO S.p.A. - All rights reserved - VAT 05412951005 Informazioni sul venditore"; }';
+    document.head.appendChild(oStyle);
+
+    this.getRouter().initialize();
+    this.setModel(models.createDeviceModel(), "device");
+
+    // Screen0/1/2: set year on vm model for XML binding
+    var self = this;
+    var fnSetYear = function () {
+        var oVm = self.getModel("vm");
+        if (oVm) {
+            oVm.setProperty("/legalYear", sYear);
+        }
+    };
+    fnSetYear();
+    this.getRouter().attachRouteMatched(fnSetYear);
+},
         });
     }
 );
